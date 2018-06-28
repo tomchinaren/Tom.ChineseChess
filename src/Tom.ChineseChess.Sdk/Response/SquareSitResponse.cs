f8ad24bd8ffc5ebdcc286e8f5df1d36c1c3f2679
liangjuntang <liangjuntang@mallcoo.cn>
2018-06-28 14:01:27 +0800

    sdk ing

---
+++ src/Tom.ChineseChess.Sdk/Response/SquareSitResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tom.ChineseChess.Sdk.Response
+{
+    public class SquareSitResponse : Tom.Api.Response.IResponse
+    {
+    }
+}
---
+++ src/Tom.ChineseChess.Sdk/Test.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tom.Api.Request;
+
+namespace Tom.ChineseChess.Sdk
+{
+    class Test
+    {
+        public static void Main()
+        {
+            IClient client = new Tom.Api.Request.DefaultClient(null, null, null, null, null, null, null);
+        }
+    }
+}
